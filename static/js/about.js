be98a3e92215f46da29c3a64746884149df8e10b
Miguel Molina <hi@mvader.me>
2016-08-29 17:24:16 +0200

    Update about.js

--- static/js/about.js
+++ static/js/about.js
@@ -5,7 +5,7 @@ function init() {
     var locations = [
       new google.maps.LatLng(40.4222722, -3.6868422),
       new google.maps.LatLng(51.5080843, -0.1095317),
-      new google.maps.LatLng(52.51074, 13.3701413),
+      new google.maps.LatLng(52.51091, 13.3701303),
     ];
 
     var mapOptions = {
