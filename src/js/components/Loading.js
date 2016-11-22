524b5a19a42d99ce601ca6de40c712bc799bffbe
Miguel Molina <hi@mvader.me>
2016-11-22 15:25:48 +0100

    Style loadings

--- src/js/components/Loading.js
+++ src/js/components/Loading.js
@@ -3,17 +3,17 @@ import React from 'react'
 export function Loading(props) {
   return (
     <div className='loading'>
-      {/* TODO: Loading GIF */}
-      <span>Loading...</span>
+      <img src='/img/loading.svg' alt="Loading" />
+      <p>Loading...</p>
     </div>
-  )  
+  )
 }
 
 export function LoadingError(props) {
   return (
     <div className='loading--error'>
-      {/* TODO: ERROR image */}
-      <span>There has been a problem loading the requested data.</span>
+      <img src='/img/error.svg' alt="Error" />
+      <p>There has been a problem loading the requested data.</p>
     </div>
   )
 }
