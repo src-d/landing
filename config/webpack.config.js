0ab51cb40bcea963dd0f3f7d14a1271adc640896
David Pordomingo <David.Pordomingo.F@gmail.com>
2016-11-23 17:20:36 +0100

    Rearrange config file to rehuse loginc base/prod/dev

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,96 +4,75 @@ let path = require('path');
 let webpack = require('webpack');
 let merge = require('webpack-merge');
 let autoprefixer = require('autoprefixer');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let extractTextPlugin = require('extract-text-webpack-plugin');
+
 let includeCompass = 'includePaths[]=' + path.resolve(__dirname, '../node_modules/compass-mixins/lib');
-let TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development';
+let IS_PRODUCTION = process.env.npm_lifecycle_event === 'build';
 
-let common = {
-  entry: [
-    __dirname + '/../src/sass/app.scss',
-    __dirname + '/../src/js/index.js',
-  ],
-
-  output: {
-    path: path.resolve(__dirname, '../static/'),
-    filename: 'js/bundle.js',
-  },
-
-  resolve: {
-    modulesDirectories: ['node_modules'],
-    extensions: ['', '.js', '.jsx']
-  },
-
-  postcss: [autoprefixer({ browsers: ['last 2 versions'] })],
-}
-
-if (TARGET_ENV === 'production') {
-  module.exports = merge(common, {
-    devtool: 'source-map',
-
-    module: {
-      loaders: [
-        {
-          test: /.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          query: {
-            presets: ['es2015', 'react']
-          }
-        }, {
-          test: /\.(eot|ttf|woff|svg|png|gif|jpg|jpeg)$/,
-          loaders: ['file']
-        }, {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style-loader', 'css!sass?' + includeCompass, ['css-loader', 'postcss-loader'])
-        }
-      ]
+let baseConf = {
+    entry: [
+        __dirname + '/../src/sass/app.scss',
+        __dirname + '/../src/js/index.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, '../static/'),
+        filename: 'js/bundle.js',
     },
-
-    plugins: [
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
-        }
-      }),
-      new ExtractTextPlugin('css/style.css', { allChunks: true }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
-    ]
-  });
-} else {
-  module.exports = merge(common, {
     module: {
-      loaders: [
+        loaders: [
         {
-          test: /.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          query: {
-            presets: ['es2015', 'react']
-          }
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
         }, {
-          test: /\.(eot|ttf|woff|svg|png|gif|jpg|jpeg)$/,
-          loaders: ['file']
+            test: /\.(eot|ttf|woff|svg|png|gif|jpg|jpeg)$/,
+            loaders: ['file']
         },
         {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('style-loader', 'css!sass?' + includeCompass, ['css-loader', 'postcss-loader'])
+            test: /\.scss$/,
+            loader: extractTextPlugin.extract(
+                'style-loader',
+                'css!sass?' + includeCompass,
+                ['css-loader', 'postcss-loader']
+            )
         }
-      ]
+        ]
     },
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js', '.jsx']
+    },
+    postcss: [autoprefixer({ browsers: ['last 2 versions'] })],
+}
 
+let productionConf = {
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new extractTextPlugin('css/style.css', { allChunks: true }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+}
+
+let developmentConf = {
     devtool: 'source-map',
     devServer: {
-      contentBase: 'static/',
+        contentBase: 'static/',
     },
-
     plugins: [
-      new ExtractTextPlugin('css/style.css', { allChunks: true }),
+        new extractTextPlugin('css/style.css', { allChunks: true }),
     ],
-  });
 }
+
+module.exports = merge(baseConf, IS_PRODUCTION ? productionConf : developmentConf);
