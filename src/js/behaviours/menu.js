40e9ae8123d06024728288e723245d5ad2a61ef4
Miguel Molina <hi@mvader.me>
2017-06-08 14:35:25 +0200

    fix slack form behaviour

--- src/js/behaviours/menu.js
+++ src/js/behaviours/menu.js
@@ -1,8 +1,11 @@
 export default function setupMenu() {
-    const menuToggle = document.querySelector('#menuToggle')
+  const menuToggle = document.querySelector('#menuToggle')
+  
+  if (menuToggle) {
     const menu = document.querySelector('#menu')
     menuToggle.addEventListener('click', _ => {
-        menu.classList.toggle('open')
-        menuToggle.classList.toggle('open')
+      menu.classList.toggle('open')
+      menuToggle.classList.toggle('open')
     })
+  }
 }
