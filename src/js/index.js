2392e43cdd9e409e3f5ad9748fd5920c110c1684
Miguel Molina <hi@mvader.me>
2017-06-09 14:37:05 +0200

    implement showcases section

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import $ from 'jquery'
+import 'slick-carousel'
 import hljs from 'highlight.js'
-import 'highlight.js/styles/atom-one-dark.css'
 import setupClipboard from './clipboard'
 import BlogPostsContainer from './components/Posts'
 import PositionsMain from './components/Positions'
@@ -22,13 +22,13 @@ function renderComponent(component, id) {
 }
 
 window.addEventListener('DOMContentLoaded', _ => {
-    setupMenu()
-    setupStickyHeader()
-    hljs.initHighlightingOnLoad()
-    renderComponent(BlogPostsContainer, 'ourPostsContainer')
-    renderComponent(PositionsMain, 'offersPanel')
-    renderComponent(SlackForm, 'slack-join')
-    setupClipboard()
+  setupMenu()
+  setupStickyHeader()
+  setupShowcases()
+  renderComponent(BlogPostsContainer, 'ourPostsContainer')
+  renderComponent(PositionsMain, 'offersPanel')
+  renderComponent(SlackForm, 'slack-join')
+  setupClipboard()
 })
 
 function setupStickyHeader() {
@@ -46,4 +46,35 @@ function checkTopbarOpacity(topBar, opaqueAtOffset) {
     } else {
         topBar.classList.remove('opaque')
     }
+}
+
+function setupShowcases() {
+  const showcase = $('.showcases__project')
+  const code = document.getElementById('showcase-code')
+  const title = document.getElementById('showcase-title')
+  showcase.slick({
+    dots: true,
+    infinite: true,
+    speed: 0,
+    fade: true,
+    swipe: false,
+    adaptiveHeight: true,
+  })
+
+  highlightCode(code)
+
+  showcase.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+    const project = window.showcases[nextSlide >= window.showcases.length ? 0 : nextSlide]
+    title.innerText = project.title
+    code.innerHTML = `
+    <pre><code class="${project.language}">${project.code}</code></pre>
+    `
+    highlightCode(code)
+  })
+}
+
+function highlightCode(code) {
+  if (code.children.length >= 0) {
+    hljs.highlightBlock(code.children[0])
+  }
 }
