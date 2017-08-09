8c18745cd47e25f07944c14402f8b5cbf9b21a52
David Pordomingo <David.Pordomingo.F@gmail.com>
2017-08-09 11:35:35 +0200

    Install Eslint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,22 @@
 module.exports = {
-  extends: "airbnb-base",
-  plugins: ["import"],
-  rules: {
+  "extends": [
+    "plugin:react/recommended",
+    "airbnb-base"
+  ],
+  "plugins": ["import", "json"],
+  "rules": {
     "import/no-extraneous-dependencies": 0,
     "import/no-unresolved": 0,
     "import/extensions": 0,
     "func-names": 0,
     "no-underscore-dangle": 0, // because of _super
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }]
+    "no-plusplus": [
+      "error",
+      { "allowForLoopAfterthoughts": true }
+    ]
+  },
+  "globals": {
+    "window": true,
+    "fetch": true
   }
 };
