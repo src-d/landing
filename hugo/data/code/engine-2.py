04b3228c961eab0493bac1727d83dafa4835a10f
Marcelo Novaes <mnovaes@gmail.com>
2017-10-18 08:48:35 +0200

    Add content with final home examples, small fix on careers OSD link
    Signed-off-by: Marcelo Novaes <mnovaes@gmail.com>

---
+++ hugo/data/code/engine-2.py
@@ -0,0 +1,27 @@
+# import the source{d} engine
+from sourced.spark import API as Engine
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+# start a new session
+spark = SparkSession.builder \
+        .master("local[*]").appName("Examples") \
+        .getOrCreate()
+
+engine = Engine(spark, "/repositories")
+
+# get all the files of all head commits
+head_files = engine.repositories.filter("is_fork = false") \
+             .references \
+             .head_ref.commits.first_reference_commit \
+             .files \
+             .classify_languages() \
+             .filter("is_binary = false") \
+             .select("file_hash", "path", "content", "lang") \
+             .filter("lang is not null").cache()
+
+# shows top languages per number of files
+top_ten_langs = head_files.distinct() \
+                .groupBy("lang").agg(count("*").alias("count")) \
+                .orderBy("count").sort(desc("count")).limit(20) \
+                .show()
